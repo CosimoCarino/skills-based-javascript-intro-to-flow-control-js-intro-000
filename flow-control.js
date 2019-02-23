8ca81b9b2d8c29e174b4aaeaf25aba32af87271b
Tess Guenthner <tessmadeleine@hotmail.com>
2019-02-23 02:49:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,10 @@
-function basicTeenager(age) { 
-  if (age < 20 && age > 12) 
+function basicTeenager(age) {
+  if (age < 20 && age > 12)
   {return "You are a teenager!"}
 }
 function teenager(age) {
   if (age < 20 && age > 12)
-  {return "You are a teenager!"} 
+  {return "You are a teenager!"}
   else {return "You are not a teenager"}
 }
 function ageChecker(age) {
@@ -16,7 +16,7 @@ function ageChecker(age) {
     {return "You are a grownup"}
   }
 function ternaryTeenager(age) {
-return age < 20 && age > 12 ? 
+return age < 20 && age > 12 ?
 "You are a teenager!":
 "You are not a teenager"
 }
