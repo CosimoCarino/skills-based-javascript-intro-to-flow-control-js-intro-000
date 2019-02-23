7ebc062256411553a8e0de4e627c9144a879c6ba
Tess Guenthner <tessmadeleine@hotmail.com>
2019-02-23 02:51:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,7 +17,7 @@ function ageChecker(age) {
   }
 function ternaryTeenager(age) {
 return age < 20 && age > 12 ?
-"You are a teenager!":
+"You are a teenager":
 "You are not a teenager"
 }
 function switchAge(age) {
